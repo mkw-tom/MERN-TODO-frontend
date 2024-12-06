f9aa3dd36025bd2cae9f917f398f77953c8500df
Tomu Maekawa <tomu.m1107@icloud.com>
2024-12-07 01:13:52 +0900

    refactor UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { FiDelete, FiEdit } from 'react-icons/fi';
 import './App.css';
 import { CgAdd } from 'react-icons/cg';
-import { LuSwatchBook } from "react-icons/lu";
-import { FaPen } from "react-icons/fa";
+import { FaPen } from 'react-icons/fa';
+import { LuSwatchBook } from 'react-icons/lu';
 
 function App() {
   const array = [
@@ -14,7 +14,10 @@ function App() {
   return (
     <div className="w-full h-screen flex items-start justify-around">
       <div className="w-1/3 h-screen text-lg py-5 px-10 border-r-2 border-info">
-        <h1 className="font-bold mb-10 flex items-center gap-1 text-center mx-auto w-28 "><FaPen></FaPen><span>メモ追加</span></h1>
+        <h1 className="font-bold mb-10 flex items-center gap-1 text-center mx-auto w-28 ">
+          <FaPen></FaPen>
+          <span>メモ追加</span>
+        </h1>
         <form className="flex flex-col gap-5 items-center ">
           <input
             className="input input-info w-[300px]"
@@ -24,11 +27,17 @@ function App() {
             className="textarea textarea-info w-[300px] h-[400px]"
             placeholder="メモ"
           ></textarea>
-          <button className="btn btn-info w-[300px] shadow-xl"><CgAdd />追加</button>
+          <button className="btn btn-info w-[300px] shadow-xl">
+            <CgAdd />
+            追加
+          </button>
         </form>
       </div>
       <div className="w-2/3 flex flex-col p-5">
-        <h1 className="font-bold text-center mb-10 flex items-center mx-auto gap-1"><LuSwatchBook></LuSwatchBook><span>メモ一覧</span></h1>
+        <h1 className="font-bold text-center mb-10 flex items-center mx-auto gap-1">
+          <LuSwatchBook></LuSwatchBook>
+          <span>メモ一覧</span>
+        </h1>
         <input
           type="text"
           className="input input-info w-[300px] mb-10"
@@ -38,8 +47,8 @@ function App() {
         <div className="grid grid-cols-2 gap-3">
           {array.map((e) => (
             <div className="flex  items-center justify-between border-2 border-info py-2 px-3 rounded-lg">
-              <h3 className='w-9/12 text-center'>{e.title}</h3>
-              <div className='flex w-3/12'>
+              <h3 className="w-9/12 text-center">{e.title}</h3>
+              <div className="flex w-3/12">
                 <div className="text-center ">
                   <div
                     className="btn hover:text-info"
