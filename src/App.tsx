d425029faa88f3d9e3833964acd737279667e96a
Tomu Maekawa <tomu.m1107@icloud.com>
2024-12-07 00:33:26 +0900

    create app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="w-full h-screen flex items-start justify-around">
       <div className="w-1/3 h-screen text-lg py-5 px-10 border-r-2 border-info">
-      <h1 className='font-bold  text-center mb-10'>メモ追加</h1>
+        <h1 className="font-bold  text-center mb-10">メモ追加</h1>
         <form className="flex flex-col gap-5 items-center ">
           <input
             className="input input-info w-[300px]"
@@ -24,20 +24,22 @@ function App() {
         </form>
       </div>
       <div className="w-2/3 flex flex-col p-5">
-      <h1 className='font-bold text-center mb-10'>メモ一覧</h1>
-          <input
-            type="text"
-            className="input input-info w-[300px] mb-10"
-            placeholder="検索"
-          />
+        <h1 className="font-bold text-center mb-10">メモ一覧</h1>
+        <input
+          type="text"
+          className="input input-info w-[300px] mb-10"
+          placeholder="検索"
+        />
 
-        <div className='grid grid-cols-2 gap-3'>
+        <div className="grid grid-cols-2 gap-3">
           {array.map((e) => (
             <div className="card ">
               <button
                 className="btn btn-outline btn-info"
                 onClick={() => {
-                  const modal = document.getElementById('my_modal_5') as HTMLDialogElement | null;
+                  const modal = document.getElementById(
+                    'my_modal_5',
+                  ) as HTMLDialogElement | null;
                   if (modal) {
                     modal.showModal();
                   }
@@ -51,9 +53,7 @@ function App() {
               >
                 <div className="modal-box border border-info">
                   <h3 className="font-bold text-lg">{e.title}</h3>
-                  <p className="py-4">
-                    {e.memo}
-                  </p>
+                  <p className="py-4">{e.memo}</p>
                   <div className="modal-action">
                     <form method="dialog">
                       {/* if there is a button in form, it will close the modal */}
