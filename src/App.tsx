10920640d933708f9e8cbda1f74a2b6b3d9a9e1f
Tomu Maekawa <tomu.m1107@icloud.com>
2024-12-06 23:51:04 +0900

    setup css

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="w-full h-full bg-black flex items-center justify-around">
       <div className="w-1/2 bg-black text-lg">dd</div>
-      <div className='w-1/2'>ddd</div>
+      <div className="w-1/2">ddd</div>
     </div>
   );
 }
